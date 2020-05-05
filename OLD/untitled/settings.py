332286615f8d3b130ce7a10a62e7fde63931fe40
sarablondal <sarablondal@users.noreply.github.com>
2020-05-05 21:13:59 +0000

    GETTNG CLEANER

--- OLD/untitled/settings.py
+++ OLD/untitled/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'untitled.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        'DIRS': [os.path.join(BASE_DIR, '../../Verklegt2/templates')]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
@@ -128,5 +128,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, '../../Verklegt2/static')
 ]
