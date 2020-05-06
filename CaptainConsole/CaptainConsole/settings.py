aff949d1ff261663c5d24529279f756086826f14
Kiddiflame <kiddiflame@gmail.com>
2020-05-06 12:20:18 +0000

    Bætti við server stillingar

--- CaptainConsole/CaptainConsole/settings.py
+++ CaptainConsole/CaptainConsole/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'CaptainConsole.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dqipfdno',
+        'USER': 'dqipfdno',
+        'PASSWORD': 'vf1pn3UbmxQlu6Rt2i2soVyaXGMXaKFR',
+        'HOST': 'balarama.db.elephantsql.com',
+        'PORT': '5432'
+
     }
 }
 
