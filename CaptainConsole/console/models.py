a58acf7eb48417b850fe9ff9fc25fcdb3227c8c6
Brekig <42963322+Brekig@users.noreply.github.com>
2020-05-06 12:10:11 +0000

    Update models.py inní console

--- CaptainConsole/console/models.py
+++ CaptainConsole/console/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ConsoleCategory(models.Model):
+    name = models.CharField(max_length=255)
+
+class Console(models.Model):
+    name = models.CharField(max_length=255),
+    description = models.CharField(max_length=999),
+    category = models.ForeignKey(ConsoleCategory, on_delete=models.CASCADE),
+    company = models.CharField(max_length=255),
+    price = models.FloatField()
