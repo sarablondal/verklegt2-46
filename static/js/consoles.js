def50b3e3c8bd8f064e1c841b61eafd583f02e92
Brekig <42963322+Brekig@users.noreply.github.com>
2020-05-11 16:21:10 +0000

    Update consoles.js

--- static/js/consoles.js
+++ static/js/consoles.js
@@ -1,55 +1,31 @@
-/* NOT ready, en komið langa leið runnar ekki, */
-console.log("ég er hér")
-$(document).ready(function() {
-        $('#searchBtn').on('click', function(e) {
+/* NOT ready, en komið langa leið runnar ekki, smá villa */
+console.log("ég er hér123")
+$(document).ready(function () {
+    $('#searchBtn').on('click', function (e) {
         e.preventDefault();
         var searchText = $('#searchBox').val();
-        $.ajax( url: {
+        $.ajax( {
             url:'/consoles?searchFilter=' + searchText,
             type: 'GET',
-            success: function(resp){
-            var newHtml = resp.data.map(d => {
-                return '<div class="container">\n' +
-                    '        <section class = "catalog-views">\n' +
-                    '            <div class="row">\n' +
-                    '                <a href="/consoles/{{ console.id }}">\n' +
-                    '                <div class= "catalog-view">\n' +
-                    '                    <div class = "catalog-item-img">\n' +
-                    '                        <img src="{{ console.consoleimage_set.first.image }}" />\n' +
-                    '                    </div>\n' +
-                    '                </div>\n' +
-                    '                </a>\n' +
-                    '            </div>\n' +
-                    '\n' +
-                    '                <div class="catalog-item-title">\n' +
-                    '                        {{ console.name }}\n' +
-                    '                    </div>\n' +
-                    '\n' +
-                    '                <!-- buttons hjá hverju item view síðu-->\n' +
-                    '                <div class= "catalog-cta">\n' +
-                    '                    <div class = "catalog-item-price">\n' +
-                    '                        {{ console.price }} $\n' +
-                    '\n' +
-                    '                <!--    <button class = "catalog-cta-select">select</button>-->\n' +
-                    '                <!--    <button class = "catalog-cta-add">add to cart</button>\n' +
-                    '                Commenta þetta út því ég aðeins að breyta til\n' +
-                    '                að testa cart og sjá hvort ég geti addað to cart-->\n' +
-                    '                        <form class="" action="cart" method="post">{% csrf_token %}\n' +
-                    '                            <p><input type="submit" value="Add To cart" class="catalog-cta-add"></p>\n' +
-                    '                        </form>\n' +
-                    '                        </div>\n' +
-                    '                </div>\n' +
-                    '\n' +
-                    '        </section>\n' +
-                    ' </div>'
+            success: function (resp) {
+                console.log(resp.data)
+                var newHtml = resp.data.map(d => {
+                    return `<div  class="console">
+                                <a href="consoles/${d.id}">
+                                <img class="consoleImage" src="${d.firstImage}">
+                                <h4>${d.name}</h4>
+                                <p>${d.price}</p>
+                                </a>
+                            </div>`
 
-            });
-            $('consoles').html(newHtml.join(''));
-            $('searchBox').val('');
+                });
+                $('.container').html(newHtml.join(''));
+                $('#searchBox').val('')
             },
             error: function(xhr, status, error) {
-            console.log(error)
+                console.log(error)
             }
         })
-        })
+    })
 })
+
