4861874cd0b5b7e247e3a633a1eacfc037b72673
Brekig <42963322+Brekig@users.noreply.github.com>
2020-05-12 16:15:04 +0000

    lagfærði smá css classes og setti klasa sem var búið að gera fyrir name og img í consoles

--- static/js/consoles.js
+++ static/js/consoles.js
@@ -10,16 +10,18 @@ $(document).ready(function () {
             success: function (resp) {
                 console.log(resp.data)
                 var newHtml = resp.data.map(d => {
-                    return `<div  class="console">
-                                <a href="consoles/${d.id}">
-                                <img class="consoleImage" src="${d.firstImage}">
-                                <h4>${d.name}</h4>
-                                <p>${d.price}</p>
+                    return `<div class ="catalog-views">
+                                <a href="/consoles/${ d.id }">
+                                    <img class="catalog-item-img" src="${ d.firstImage }" />
+                                    <h3> ${ d.name } </h3>
+                                    <h4> ${ d.price } $ </h4>
                                 </a>
+                                <form class="" action="cart" method="post">
+                                    <p><input type="submit" value="add to cart" class="catalog-cta-add"></p>
+                                </form>
                             </div>`
-
                 });
-                $('.container').html(newHtml.join(''));
+                $('.catalog-views').html(newHtml.join(""));
                 $('#searchBox').val('')
             },
             error: function(xhr, status, error) {
@@ -28,4 +30,29 @@ $(document).ready(function () {
         })
     })
 })
+/*
+function saveText() {
+    console.log("saved to localStorage")
+    var e = document.getElementById("searchBox");
+    if(typeof(Storage) !== 'undefined') {
+        var currentValue = localStorage.getItem('searchField');
+        if (currentValue.length == 0) {
+            currentValue = e.value;
+        } else {
+            currentValue = currentValue + '\n' + e.value;
+        }
+        localStorage.setItem('searchField', currentValue);
+        alert('current value:\n\n' + localStorage.getItem('searchField'));
+    } else {
+        alert('Cannot access local storage.');
+    }
+} */
+
+                            /*<div  class="console">
+                                <a href="http://127.0.0.1:8000/consoles/${d.id}">
+                                <img class="consoleImage" src="${d.firstImage}">
+                                <h4>${d.name}</h4>
+                                <p>${d.price}</p>
+                                </a>
+                            </div>*/
 
