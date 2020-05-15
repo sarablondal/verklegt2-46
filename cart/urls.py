0775d5d84224bec3724c9f52166fdc4c8209dcbf
Kiddiflame <kiddiflame@gmail.com>
2020-05-15 01:03:16 +0000

    Byrjaði að laga checkout

--- cart/urls.py
+++ cart/urls.py
@@ -12,8 +12,10 @@ urlpatterns = [
 
 	path('checkout/', views.checkout, name="checkout"),
 	path('checkout2/', views.checkout2, name="checkout2"),
+	path('checkout3/', views.checkout3, name="checkout3"),
+	path('checkout4/', views.checkout4, name="checkout4"),
 
 
 	path('update_item/', views.updateItem, name="update_item"),
-	path('process_order/', views.processOrder, name="process_order"),
+	path('process_order/', views.processOrder, name="process_order")
 ]
