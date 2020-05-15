56dfd25a572c6d0599464e7dda5e840d161f43cf
Kiddiflame <kiddiflame@gmail.com>
2020-05-15 21:52:33 +0000

    Revert "Kláraði error-handling"
    This reverts commit 5537dd5993b30b32cebad348dd165dc04e280ee3.

--- errorhandling/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from . import views
-"""
-handler400 = 'errorhandling.views.view_400'
-handler403 = 'errorhandling.views.view_403'
-handler404 = 'errorhandling.views.view_404'
-handler500 = 'errorhandling.views.view_500'
-"""
