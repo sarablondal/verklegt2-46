d0bb21e0123c5f5c727fbd38c26d0c422724ba63
sarablondal <45676261+sarablondal@users.noreply.github.com>
2020-05-15 07:40:45 +0000

    listinn inní inspect fyrir contact info virkar og heldur utan um gögnin

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -1,51 +1,64 @@
-var total = '{{order.get_cart_total|floatformat:2}}'
+    var total = '{{order.get_cart_total|floatformat:2}}'
 
-
-if (user != 'AnonymousUser'){
-    document.getElementById('user-info').innerHTML = ''
- }
-
-var form = document.getElementById('form')
-form.addEventListener('click', function(e){
-    e.preventDefault()
-    console.log('Form Submitted...')
-    document.getElementById('form-button').classList.add("hidden");
+    if (user != 'AnonymousUser'){
+        document.getElementById('user-info').innerHTML = ''
+     }
+/*else {
+    document.getElementById('form-wrapper').classList.add("hidden");
     document.getElementById('payment-info').classList.remove("hidden");
+}
+
+var form = document.getElementById('form')*/
+
+$(document).ready(function() {
+    $('#submitInfo').on('click', function(e) {e.preventDefault()
+    console.log('It has been formally submitted...')
+        var fName = $('#firstName').val()
+        var lName = $('#lastName').val()
+        var email = $('#email').val()
+        var address = $('#address').val()
+        var address2 = $('#address2').val()
+        var country = $('#country').val()
+        var city = $('#city').val()
+        var zipCode = $('#zip').val()
+
+        var userInfo = {
+            'firstName': fName,
+            'lastName': lName,
+            'email': email,
+            'address': address,
+            'address2': address2,
+            'country': country,
+            'city': city,
+            'zip': zipCode
+        }
+    console.log(userInfo)
+    })
+    })
+
+
+    //form.addEventListener('submit',  function(e){e.preventDefault()
+ //   console.log('It has been formally submitted...')
+    //document.getElementById('form-button').classList.add("hidden");
+    /*document.getElementById('payment-info').classList.remove("hidden");
 })
     document.getElementById('make-payment').addEventListener('click', function(e){
         submitFormData()
-    })
+}) */
 
 function submitFormData(){
-    console.log('Payment button clicked')
-
+    console.log('You are continuing on your checkout journey!')
     var userFormData = {
-        'firstName':null,
-        'lastName':null,
-        'email':null,
-        'address': null,
-        'address2': null,
-        'country': null,
-        'city': null,
-        'zip': null,
+				'name':null,
+				'email':null,
+				'total':total,
+			}
 
-        'total':total,
-    }
-
-    if (user == 'AnonymousUser'){
-        userFormData.name = form.name.value
-        userFormData.email = form.email.value
-    }
-    else{
-        userFormData.firstName = form.firstName.value
-        userFormData.lastName = form.lastName.value
-        userFormData.email = form.email.value
-        userFormData.address = form.address.value
-        userFormData.address2 = form.address2.value
-        userFormData.country = form.country.value
-        userFormData.city = form.city.value
-        userFormData.zip = form.zip.value
-    }
+	    	if (user == 'AnonymousUser'){
+	    		userFormData.name = form.name.value
+	    		userFormData.email = form.email.value
+	    	}
+}
 
     console.log('User Info:', userFormData)
 
@@ -56,7 +69,7 @@ function submitFormData(){
             'Content-Type':'application/json',
             'X-CSRFToken':csrftoken,
         },
-        body:JSON.stringify({'form':userFormData, 'shipping':shippingInfo}),
+        body:JSON.stringify({'form':userFormData }),
     })
         .then((response) => response.json())
         .then((data) => {
@@ -66,4 +79,3 @@ function submitFormData(){
             document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
             window.location.href = "{% url 'cart' %}"
         })
-}
